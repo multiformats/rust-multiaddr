c646bb16c276d0dcf64f013a7ee7fd8960885430
dignifiedquire <dignifiedquire@gmail.com>
2015-12-13 19:35:34 +0100

    nom: from string parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-extern crate byteorder;
+#[macro_use]
 extern crate nom;
+
+extern crate byteorder;
 
 pub use self::protocol_types::*;
 pub mod protocol_types;
@@ -7,39 +9,66 @@ pub mod protocol_types;
 use std::io::Cursor;
 use std::cmp::PartialEq;
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
-
+use nom::IResult;
 
 pub struct Multiaddr {
     bytes: Vec<u8>
 }
 
-fn parse_addr(input: &[u8]) -> IResult<&[u8], Multiaddr> {
-    chain!(input,
+/// Parse a single /
+named!(sep, tag!("/"));
 
-           )
-}
+/// Parse a single multiaddress in the form of `/ip4/127.0.0.1`.
+named!(address <&[u8], (&str, &str)>,
+    chain!(
+         opt!(sep)             ~
+      t: map_res!(
+          take_until!("/"),
+          std::str::from_utf8
+         )                     ~
+         sep                   ~
+      a: map_res!(
+          is_not!("/"),
+          std::str::from_utf8
+         ),
+      || {(t, a)}
+    )
+);
+
+/// Parse a list of addresses
+named!(addresses < &[u8], Vec<(&str, &str)> >, many1!(address));
 
 impl Multiaddr {
     /// Create a new multiaddr based on a string representation, like
     /// `/ip4/127.0.0.1/udp/1234`.
-    pub fn new(address: &str) -> Multiaddr {
-        let address = address.to_string();
+    pub fn new(input: &str) -> Multiaddr {
+        match addresses(input.as_bytes()) {
+            IResult::Done(i, tuple_vec) => {
+                println!("Not yet parsed {}", std::str::from_utf8(i).unwrap());
+                println!("found {} addresse(s)", tuple_vec.len());
+                for el in &tuple_vec {
+                    println!("{}, {}", el.0, el.1);
+                }
+            },
+            _ => println!("error")
+        }
+
         let mut bytes: Vec<u8>= vec![];
 
-        for part in address.split("/") {
-            if let Some(protocol) = ProtocolTypes::from_name(part) {
-                bytes.write_u16::<LittleEndian>(protocol.to_code());
-                println!("Bytes {:?}", bytes);
-                let mut rdr = Cursor::new(vec![4, 0]);
-                println!("Bytes reversed {:?}", rdr.read_u16::<LittleEndian>());
-                println!("Got protocol {}", protocol.to_name());
-                println!("With size {}", protocol.to_size());
-            } else if part.len() > 0 {
-                let mut part_bytes = part.to_string().into_bytes();
-                bytes.append(&mut part_bytes);
-                println!("got part {}, {}", part.len(), part);
-            }
-        }
+        // for part in address.split("/") {
+        //     if let Some(protocol) = ProtocolTypes::from_name(part) {
+        //         bytes.write_u16::<LittleEndian>(protocol.to_code());
+        //         println!("Bytes {:?}", bytes);
+        //         let mut rdr = Cursor::new(vec![4, 0]);
+        //         println!("Bytes reversed {:?}", rdr.read_u16::<LittleEndian>());
+        //         println!("Got protocol {}", protocol.to_name());
+        //         println!("With size {}", protocol.to_size());
+        //     } else if part.len() > 0 {
+        //         let mut part_bytes = part.to_string().into_bytes();
+        //         bytes.append(&mut part_bytes);
+        //         println!("got part {}, {}", part.len(), part);
+        //     }
+        // }
 
         Multiaddr {
             bytes: bytes,
