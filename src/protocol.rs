b4f0a3f31822ad66d23233083dd024627472b153
Thomas Eizinger <thomas@eizinger.io>
2023-02-27 18:01:39 +1100

    Don't depend on unnecessary features

--- src/protocol.rs
+++ src/protocol.rs
@@ -3,7 +3,7 @@ use crate::{Error, Result};
 use arrayref::array_ref;
 use byteorder::{BigEndian, ByteOrder, ReadBytesExt, WriteBytesExt};
 use data_encoding::BASE32;
-use multihash::Multihash;
+use multihash::MultihashGeneric;
 use std::{
     borrow::Cow,
     convert::From,
@@ -51,6 +51,8 @@ const WS: u32 = 477;
 const WS_WITH_PATH: u32 = 4770; // Note: not standard
 const WSS: u32 = 478;
 const WSS_WITH_PATH: u32 = 4780; // Note: not standard
+
+type Multihash = MultihashGeneric<64>;
 
 const PATH_SEGMENT_ENCODE_SET: &percent_encoding::AsciiSet = &percent_encoding::CONTROLS
     .add(b'%')
